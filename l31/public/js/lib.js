1f4394bca1c826c3f08a87181532ffab23171175
thienth32 <thienth32@gmail.com>
2017-11-18 08:43:28 +0700

    update

--- l31/public/js/lib.js
+++ l31/public/js/lib.js
@@ -28,5 +28,22 @@ window.lib = {
 		}	
 		// hien thi ket qua 
 		console.log(result+value);
+	},
+	b5: function(){
+		var result = "";
+		// lay gia tri tu input
+		var value1 = this.txt.value;
+		// lay gia tri tu prompt
+		var value2 = prompt('Nhap gia tri can xoa');
+		// tim vi tri xuat hien cua value2 trong value1
+		var index = value1.indexOf(value2);
+		var endIndex = index + value2.length;
+		// neu index != -1 thi xoa value2 trong value1
+		if(index != -1){
+			result = value1.slice(0, index); 
+			result += value1.slice(endIndex, value1.length);
+		}
+
+		console.log(result);
 	}
 }
