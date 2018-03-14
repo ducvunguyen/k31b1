87eba85cc7b1036b9d0b02963317d56642c95af6
thienth32 <thienth32@gmail.com>
2018-03-14 10:54:14 +0700

    update

--- l69/app/views/admin/category/list.php
+++ l69/app/views/admin/category/list.php
@@ -6,11 +6,7 @@
 
           <div class="box-tools">
             <div class="input-group input-group-sm" style="width: 150px;">
-              <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
-
-              <div class="input-group-btn">
-                <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-              </div>
+              
             </div>
           </div>
         </div>
@@ -22,7 +18,9 @@
               <th>STT</th>
               <th>Tên danh mục</th>
               <th>Ảnh</th>
-              <th></th>
+              <th>
+                <a href="category-add.php" class="btn btn-sm btn-success">Add</a>
+              </th>
             </tr>
             <?php foreach ($cates as $index => $c): ?>
             	<tr>
